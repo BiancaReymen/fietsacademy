254360eea25a0fd1f9c87ff7d2a3dbd0178a61a9
Bianca Reymen <Bianca.Reymen@pandora.be>
2018-11-27 14:35:06 +0100

    hoofdstuk 6

--- src/main/java/be/vdab/fietsacademy/entities/Docent.java
+++ src/main/java/be/vdab/fietsacademy/entities/Docent.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import be.vdab.fietsacademy.enums.Geslacht;
 
 @Entity
 @Table(name = "docenten")
@@ -17,7 +21,13 @@ public class Docent implements Serializable {
 	private String familienaam;
 	private BigDecimal wedde;
 	private String emailAdres;
+	//enum
+	@Enumerated(EnumType.STRING)
+	private Geslacht geslacht;
 	
+	public Geslacht getGeslacht() {
+		return geslacht;
+	}
 	public long getId() {
 		return id;
 	}
--- src/test/java/be/vdab/fietsacademy/repositories/JpaDocentRepositoryTest.java
+++ src/test/java/be/vdab/fietsacademy/repositories/JpaDocentRepositoryTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.junit4.SpringRunner;
 
 import be.vdab.fietsacademy.entities.Docent;
+import be.vdab.fietsacademy.enums.Geslacht;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -28,6 +29,9 @@ public class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringCo
 	private long idVanTestMan() {
 		return super.jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testM'", Long.class);
 	}
+	private long idVanTestVrouw() {
+		return super.jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testV'", Long.class);
+	}
 	
 	@Test
 	public void read() {
@@ -37,6 +41,14 @@ public class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringCo
 	@Test
 	public void readOnbestaandeDocent() {
 		assertFalse(repository.read(-1).isPresent());
+	}
+	@Test
+	public void man() {
+		assertEquals(Geslacht.MAN, repository.read(idVanTestMan()).get().getGeslacht());
+	}
+	@Test
+	public void vrouw() {
+		assertEquals(Geslacht.VROUW, repository.read(idVanTestVrouw()).get().getGeslacht());
 	}
 }
 
